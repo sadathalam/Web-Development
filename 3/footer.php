793ed18075a0084c6c4829290a31636a5d71a974
sadath alam <70308258+sadathalam@users.noreply.github.com>
2020-10-27 04:32:12 -0400

    Add files via upload

--- 3/footer.php
+++ 3/footer.php
@@ -9,8 +9,8 @@ Thank you for using our site.
 	
 Results and page (c) Copyright NerdLuv Inc. </pre> </p>
 <a href="https://codd.cs.gsu.edu/~salam12/WP/HW/3/index.php"> <img src="backtofront.png" width="150" height="30" alt="back"> </a><br><br><br>
-<img src="w3chtml.png" width="70" height="50" alt="w3ht">
-<img src="w3ccss.png" width="70" height="50" alt="w3cs">
+<a href="https://validator.w3.org/"><img src="w3chtml.png" width="70" height="50" alt="w3ht"></a>
+<a href="https://jigsaw.w3.org/css-validator/"><img src="w3ccss.png" width="70" height="50" alt="w3cs"></a>
 </footer>
 </body>
 </html>
--- 3/matches-submit.php
+++ 3/matches-submit.php
@@ -33,19 +33,85 @@
 	
 	foreach($string_ar as $val){
 		$arr = explode(",",$val);
-		if($userGender != $arr[1]){
+		if(($userGender != $arr[1]) && ($userOS == $arr[4]) ){
 			array_push($alike,$val);
 		}
-		if((int)$arr[2] > (int)$userMaxAge){
-			unset($alike[$val]);
-		}
+		
+		
 	}
-	
-		print_r($alike);
+	$alike2 = array();
+	foreach($alike as $val){
+		$a = explode(",",$val);
+		$comparisonPersonality = $a[3]; 
+		$str1=substr($comparisonPersonality,0,1);
+		$str2=substr($comparisonPersonality,1,2);
+		$str3=substr($comparisonPersonality,2,3);
+		$str4=substr($comparisonPersonality,3,4);
+	if((substr($userPersonality,0,1) == $str1)||(substr($userPersonality,1,2) == $str2)||(substr($userPersonality,2,3) == $str3)||(substr($userPersonality,3,4) == $str4)){
+			array_push($alike2,$val);
+			
+		}
+		 
+		}
+		$alike3 = array();
+	foreach($alike2 as $val){
+		$a = explode(",",$val);
+		$comparisonAge= $a[2];
+		if(((int)$comparisonAge>=(int)$userMinAge) && ((int)$comparisonAge<=(int)$userMaxAge)){
+		array_push($alike3,$val);
+		}	
+	}
+	$gender = "gender: ";
+	$age = "age: "; 
+	$type = "type: "; 
+	$os = "OS: "; 
+		
+	foreach($alike3 as $val){
+		$a = explode(",",$val);
+		
+		echo "<div class=\"match\">";
+		echo "<img  src=\"user.jpg\" alt=\"userpic\">";
+		echo "<ul>";
+		echo "<li><p>";
+		echo printName($a);
+		echo "</p></li>";
+		echo "<li> $gender";
+		echo printGender($a);
+		echo "</li>";
+		echo "<li> $age";
+		echo printAge($a);
+		echo "</li>";
+		echo "<li> $type";
+		echo printType($a);
+		echo "</li>";
+		echo "<li> $os";
+		echo printOs($a);
+		echo "</li>";
+		echo "</ul>";
+		echo "</div>";
+	}
+		
 		
 		
 	
 	?>
+	<?php
+function printName($arr){
+	echo $arr[0]; 
+}
+function printGender($arr){
+	echo $arr[1];
+}
+function printAge($arr){
+	echo $arr[2];
+}
+function printType($arr){
+	echo $arr[3];
+}
+function printOs($arr){
+	echo $arr[4];
+}
+?>
 	
 	<?php 
 	include("footer.php");
